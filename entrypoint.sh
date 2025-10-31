7f38f0350a3ec02ffff34abfd03283713b5de033
jaredleechrstians <jaredleechristians@gmail.com>
2025-10-31 23:09:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-if [ "${NODE_ENV:-production}" = "development" ]; then
+if [ "${APP_DEBUG:-false}" = "true" ]; then
   echo "Starting Nuxt in development mode..."
   exec npm run dev
 else
-  echo "Starting Nuxt in production mode..."
+  echo "Starting Nuxt in production mode..."  
   exec node .output/server/index.mjs
 fi
-
-
